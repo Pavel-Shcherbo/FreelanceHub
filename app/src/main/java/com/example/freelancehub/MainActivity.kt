a788cabcedb4196c52ebec3b011673c1826f6fb1
Pavel-Shcherbo <p.shcherbo87@gmail.com>
2024-09-26 19:29:03 +0300

    Update proj

--- app/src/main/java/com/example/freelancehub/MainActivity.kt
+++ app/src/main/java/com/example/freelancehub/MainActivity.kt
@@ -5,10 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,10 +16,5 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        showWelcomeMessage()
-    }
-
-    private fun showWelcomeMessage() {
-        Toast.makeText(this, "Welcome to FreelanceHub!", Toast.LENGTH_SHORT).show()
     }
 }
